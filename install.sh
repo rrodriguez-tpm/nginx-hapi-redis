026447a0b44faa1ee8d4f86b60a8e60b211c392f
rrodriguez-tpm <117754431+rrodriguez-tpm@users.noreply.github.com>
2023-01-05 15:31:48 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
-# takes two paramters, the domain name and the email to be associated with the certificate
-USER=$1
-DOMAIN=$2
-EMAIL=$3
+if [ $# -ne 3 ]; then
+   echo 'Error'
+   echo 'Requires 3 parameters: $USER domain-name email'
+   exit 1
+else
+# takes three paramters, current user, the domain name and the email to be associated with the certificate
+   USER=$1
+   DOMAIN=$2
+   EMAIL=$3
 
-echo DOMAIN=${DOMAIN} >> .env
-echo EMAIL=${EMAIL} >> .env
+   echo DOMAIN=${DOMAIN} >> .env
+   echo EMAIL=${EMAIL} >> .env
 
 # Phase 1
-docker-compose -f ./docker-compose-initiate.yaml up -d nginx
-docker-compose -f ./docker-compose-initiate.yaml up certbot
-docker-compose -f ./docker-compose-initiate.yaml down
+   docker-compose -f ./docker-compose-initiate.yaml up -d nginx
+   docker-compose -f ./docker-compose-initiate.yaml up certbot
+   docker-compose -f ./docker-compose-initiate.yaml down
 
-echo 'Starting some configurations for lets encrypt'
+   echo 'Starting some configurations for lets encrypt'
 # some configurations for let's encrypt
-curl -L --create-dirs -o etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
-openssl dhparam -out etc/letsencrypt/ssl-dhparams.pem 2048
+   curl -L --create-dirs -o etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
+   openssl dhparam -out etc/letsencrypt/ssl-dhparams.pem 2048
 
 # Phase 2
-crontab -u $USER ./etc/crontab
-docker-compose -f ./docker-compose.yaml up -d
-
+   crontab -u $USER ./etc/crontab
+   docker-compose -f ./docker-compose.yaml up -d
+fi
